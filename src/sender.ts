a981dd2baf5ddacb413490792691a7b3ad646c82
xstoicunicornx <xstoicunicornx@users.noreply.github.com>
2026-03-04 01:48:39 -0600

    adds really rough flow for sending original psbt and polling for proposal psbt

--- src/sender.ts
+++ src/sender.ts
@@ -1,6 +1,7 @@
 import { payjoin, uniffiInitAsync } from "@xstoicunicornx/payjoin_test";
 import { originalPsbt } from "payjoin-test-utils";
 import Client from "bitcoin-core";
+import { postPjRequest } from "./utils";
 
 const rpcuser = "admin1";
 const rpcpassword = "123";
@@ -63,14 +64,67 @@ export class Sender {
   }
 
   async walletcreatefundedpsbt(address: string, amount: bigint) {
-    const amount_btc = Number(amount) / 100000000;
-    const psbt = await this.walletCommand("walletcreatefundedpsbt", [
+    const amountBtc = Number(amount) / 100000000;
+    const { psbt } = await this.walletCommand("walletcreatefundedpsbt", [
       [], // inputs
-      [{ [address]: amount_btc }], // outputs
+      [{ [address]: amountBtc }], // outputs
       0, // locktime
       { fee_rate: 1 }, // options
     ]);
-    console.log("psbt", psbt);
+    console.log("walletcreatefundedpsbt", psbt);
     return psbt;
+  }
+
+  async walletprocesspsbt(unsignedPsbt: string) {
+    const { psbt } = await this.walletCommand("walletprocesspsbt", [
+      unsignedPsbt,
+      true,
+      "ALL",
+      false,
+    ]);
+    console.log("walletprocesspsbt", psbt);
+    return psbt;
+  }
+
+  postRequest(request: payjoin.Request) {
+    return fetch(request.url, {
+      method: "POST",
+      headers: { "Content-Type": request.contentType },
+      body: request.body,
+    });
+  }
+
+  async getNewPayjoinSender(uri: string) {
+    try {
+      const pjUri = payjoin.Uri.parse(uri).checkPjSupported();
+      console.log("pjuri", pjUri.pjEndpoint());
+      const address = pjUri.address();
+      const amount = pjUri.amountSats();
+      const unsignedPsbt = await this.walletcreatefundedpsbt(address, amount);
+      const psbt = await this.walletprocesspsbt(unsignedPsbt);
+      const payjoinSender = await new payjoin.SenderBuilder(psbt, pjUri)
+        .buildRecommended(BigInt(1))
+        .saveAsync(this.persister);
+      const relayIndex = Math.floor(Math.random() * ohttpRelays.length);
+      const { request, ohttpCtx } = payjoinSender.createV2PostRequest(
+        ohttpRelays[relayIndex],
+      );
+      // postPjRequest(request);
+      const response = await this.postRequest(request);
+      console.log("response", response);
+
+      const payjoinSender2 = await payjoinSender
+        .processResponse(await response.arrayBuffer(), ohttpCtx)
+        .saveAsync(this.persister);
+      const { request: getRequest, ohttpCtx: ohttpCtx2 } =
+        payjoinSender2.createPollRequest(ohttpRelays[relayIndex]);
+      const response2 = await this.postRequest(getRequest);
+      const result = await payjoinSender2
+        .processResponse(await response2.arrayBuffer(), ohttpCtx2)
+        .saveAsync(this.persister);
+      console.log("result", result);
+    } catch (err) {
+      console.log("error", err);
+    }
   }
 }
